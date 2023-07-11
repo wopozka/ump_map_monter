ec1da3ae6b3451607f7854c1eafc42b40a31688c
Piotr Wawrzyniak <wopozka@gmail.com>
2023-07-11 12:34:50 +0200

    korekta bledow

--- mont_demont.py
+++ mont_demont.py
@@ -64,7 +64,7 @@ class Mkgmap(object):
             pass
         else:
             if int(self.args.max_jobs):
-                java_call_args += ['--max-jobs=' + args.max_jobs]
+                java_call_args += ['--max-jobs=' + self.args.max_jobs]
         nazwa_map = 'UMP mkgmap ' + date.today().strftime('%d%b%y')
         java_call_args += ['--family-name=' + nazwa_map, '--series-name=' + nazwa_map]
         plik_licencji = os.path.join(os.path.join(self.zmienne.KatalogzUMP, 'narzedzia'), 'UMP_mkgmap_licencja.txt')
@@ -3039,7 +3039,7 @@ def zwroc_wlasne_definicje_aliasow(definicje_aliasow_z_pliku, err_out_writer):
         try:
             def_aliasu = json.loads(definicja_aliasu)
         except json.decoder.JSONDecodeError:
-            perr_out_writer.stderrwrite('Niepoprawna linia w pliku z aliasami:')
+            err_out_writer.stderrwrite('Niepoprawna linia w pliku z aliasami:')
             err_out_writer.stderrwrite(str(definicja_aliasu))
             continue
         if dozwolone_klucze.difference(a for a in def_aliasu):
@@ -3539,7 +3539,8 @@ def demontuj(args):
         else:
             try:
                 if nazwa_pliku.find('granice-czesciowe.txt') > 0:
-                    orgPlikZawartosc = open(nazwa_pliku, encoding=Zmienne.Kodowanie, errors=Zmienne.ReadErrors).readlines()
+                    orgPlikZawartosc = open(nazwa_pliku, encoding=Zmienne.Kodowanie,
+                                            errors=Zmienne.ReadErrors).readlines()
                 else:
                     orgPlikZawartosc = open(os.path.join(Zmienne.KatalogzUMP, nazwa_pliku), encoding=Zmienne.Kodowanie,
                                             errors=Zmienne.ReadErrors).readlines()
@@ -3981,7 +3982,7 @@ def wojkuj(args):
     wynik_mp = os.path.join(Zmienne.KatalogRoboczy, Zmienne.InputFile)
     mapa_woj_mp = os.path.join(os.path.join(Zmienne.KatalogzUMP, 'narzedzia'), 'mapka_woj.mp')
     wynik_mp_wojek = tempfile.NamedTemporaryFile('w', encoding=Zmienne.Kodowanie, dir=Zmienne.KatalogRoboczy,
-                                                   delete=False)
+                                                 delete=False)
     wynik_mp_wojek.close()
     wojek_call = [wojek_exe, '-s', wojek_slownik_txt, '-k', mapa_woj_mp, '-f', wynik_mp, '-F', wynik_mp_wojek.name]
     print(' '.join(wojek_call))
@@ -4272,29 +4273,34 @@ def main(argumenty):
     parsers_listuj.set_defaults(func=listujobszary)
 
     # parser dla komendy zapiszkonf
-    parsers_zapiszkonf = subparsers.add_parser('zapiszkonf', help='tworzy plik konfiguracyjny z katalogiem domowym ump i katalogiem roboczym')
+    parsers_zapiszkonf = subparsers.add_parser('zapiszkonf', help='tworzy plik konfiguracyjny z katalogiem domowym ump '
+                                                                  'i katalogiem roboczym')
     parsers_zapiszkonf.set_defaults(func=zapiszkonfiguracje)
 
     # parser dla komendy edytuj - uruchamianie mapedit
     parsers_edytuj = subparsers.add_parser('edytuj', help='uruchom mapedit')
-    parsers_edytuj.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do demontazu, domyslnie wynik.mp')
+    parsers_edytuj.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do demontazu, '
+                                                                          'domyslnie wynik.mp')
     parsers_edytuj.add_argument('-me2', '--mapedit-2', action='store_true', dest='mapedit2',
                                 help='alternatywna wersja mapedit, zdefiniowana w konfiguracji')
     parsers_edytuj.set_defaults(func=edytuj)
 
     # parser dla komendy sprawdz - sprawdza negenem
     parsers_sprawdz = subparsers.add_parser('sprawdz', help='sprawdza siatke drog netgenem')
-    parsers_sprawdz.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do sprawdzenia, domyslnie wynik.mp')
+    parsers_sprawdz.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do sprawdzenia, '
+                                                                           'domyslnie wynik.mp')
     parsers_sprawdz.set_defaults(func=sprawdz)
 
     # parser dla komendy sprawdz_numeracje
     parsers_sprawdz = subparsers.add_parser('sprawdz_numeracje', help='sprawdza numeracje')
-    parsers_sprawdz.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do sprawdzenia, domyslnie wynik.mp')
+    parsers_sprawdz.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do sprawdzenia, '
+                                                                           'domyslnie wynik.mp')
     parsers_sprawdz.set_defaults(func=sprawdz_numeracje)
 
     # parser dla komendy sprawdz_siatke_routingowa
     parsers_sprawdz = subparsers.add_parser('sprawdz_siatke_routingowa', help='sprawdz ciaglosc siatki routingowej')
-    parsers_sprawdz.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do sprawdzenia, domyslnie wynik.mp')
+    parsers_sprawdz.add_argument('-i', '--input-file', dest='plikmp', help='nazwa pliku do sprawdzenia, '
+                                                                           'domyslnie wynik.mp')
     parsers_sprawdz.add_argument('-m', '--mode', dest='mode', default='sprawdz_siatke_dwukierunkowa',
                                  choices=['sprawdz_siatke_dwukierunkowa', 'sprawdz_siatke_jednokierunkowa'],
                                  help='sprawdz siatke routingowa dwukierunkowa i jednokierunkowa')
@@ -4342,7 +4348,7 @@ def main(argumenty):
     # parser dla komendy wojkuj
     parser_wojkuj_mape = subparsers.add_parser('wojkuj', help="Dodaj dane przy pomocy wojka")
     parser_wojkuj_mape.add_argument('-i', '--input-file', dest='plikmp', default=['wynik.mp'], nargs=1,
-                 help='Nazwa pliku do przetworzenia. Domyslnie wynik.mp')
+                                    help='Nazwa pliku do przetworzenia. Domyslnie wynik.mp')
     parser_wojkuj_mape.set_defaults(func=wojkuj)
 
     # parser dla komendy kompiluj mape
@@ -4350,12 +4356,13 @@ def main(argumenty):
     parser_kompiluj_mape.add_argument('-m', '--mkgmap-path', default='', help='Sciezka do programu mkgmap')
     parser_kompiluj_mape.add_argument('-fi', '--family-id', default='6324', help='family id mapy, domyslnie 6324')
     parser_kompiluj_mape.add_argument('-cp', '--code-page', default='cp1250', choices=['cp1250', 'ascii'],
-                                      help='kodowanie pliku: cp1250 - z polskimi literkami, ascii - bez polskich literek')
+                                      help='kodowanie pliku: cp1250 - z polskimi literkami, ascii - bez '
+                                           'polskich literek')
     parser_kompiluj_mape.add_argument('-nt', '--nazwa-typ', default='domyslny',
                                       choices=['brak', 'domyslny', 'rzuq', 'olowos', 'reczniak'],
                                       help='wybierz plik typ dla mapy - domyslny jest uzywany standardowo')
     parser_kompiluj_mape.add_argument('-uw', '--uwzglednij-warstwice', default=False, action='store_true',
-                                     help='Dodaj warstwice do pliku mapy')
+                                      help='Dodaj warstwice do pliku mapy')
     parser_kompiluj_mape.add_argument('-fm', '--format-mapy', default='gmapsupp', choices=['gmapsupp', 'gmapi'],
                                       help="Generuj mape w formacie gmapsupp.img albo gmapii. "
                                            "Gmapsupp wgrywasz do odbiornika, gmapi wgrywasz do mapsource/basecamp.")
@@ -4377,12 +4384,13 @@ def main(argumenty):
     parser_kompiluj_typ.add_argument('-m', '--mkgmap-path', default='', help='Sciezka do programu mkgmap')
     parser_kompiluj_typ.add_argument('-Xmx', '--maksymalna-pamiec', default='1G',
                                       help='Maksymalna pamiêc dla srodowiska java, np -Xmx 2G gdzie g, G, m, M,')
-    parser_kompiluj_typ.add_argument('-f', '--family-id', default='6324', help='Family ID dla pliku typ'
-                                                                                'domyslnie 6324')
+    parser_kompiluj_typ.add_argument('-f', '--family-id', default='6324', help='Family ID dla pliku typ '
+                                                                               '(domyslnie 6324')
     parser_kompiluj_typ.add_argument('-w', '--uwzglednij-warstwice', default=False, action='store_true',
                                      help='Dodaj warstwice do pliku typ')
     parser_kompiluj_typ.add_argument('-cp', '--code-page', default='cp1250', choices=['cp1250', 'ascii'],
-                                     help='kodowanie pliku: cp1250 - z polskimi literkami, ascii - bez polskich literek')
+                                     help='kodowanie pliku: cp1250 - z polskimi literkami,'
+                                          ' ascii - bez polskich literek')
     parser_kompiluj_typ.set_defaults(func=stworz_plik_typ)
 
     args = parser.parse_args()
