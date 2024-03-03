8a055be543e04cbb234e9ce05ce7ca82b1058661
Piotr Wawrzyniak <wopozka@gmail.com>
2024-03-03 09:53:51 +0100

    koordynacja z cvs

--- mdmMp2xml.py
+++ mdmMp2xml.py
@@ -311,29 +311,29 @@ runstamp = ''
 umppline_types = dict()
 
 pline_types = {
-    0x1:  ["highway",  "motorway"],
-    0x2:  ["highway",  "trunk"],
-    0x3:  ["highway",  "primary"],
-    0x4:  ["highway",  "secondary"],
-    0x5:  ["highway",  "tertiary"],
-    0x6:  ["highway",  "residential"],
-    0x7:  ["highway",  "living_street", "note", "FIXME: select one of: living_street, service, residential"],
-    0x8:  ["highway",  "trunk_link"],
-    0x9:  ["highway",  "motorway_link"],
-    0xa:  ["highway",  "track", "tracktype", "grade2", "access", "yes"],
-    0xb:  ["highway",  "primary_link"],
-    0xc:  ["junction", "roundabout"],
+    0x1:  ["highway",  "motorway", "ump:osmandpri", "1.1"],
+    0x2:  ["highway",  "trunk", "ump:osmandpri", "1.0"],
+    0x3:  ["highway",  "primary", "ump:osmandpri", "1.0"],
+    0x4:  ["highway",  "secondary", "ump:osmandpri", "0.95"],
+    0x5:  ["highway",  "tertiary", "ump:osmandpri", "0.9"],
+    0x6:  ["highway",  "residential", "ump:osmandpri", "0.7"],
+    0x7:  ["highway",  "living_street", "ump:osmandpri", "0.5", "note", "FIXME: select one of: living_street, service, residential"],
+    0x8:  ["highway",  "trunk_link", "ump:osmandpri", "0.7"],
+    0x9:  ["highway",  "motorway_link", "ump:osmandpri", "0.7"],
+    0xa:  ["highway",  "track", "tracktype", "grade2", "access", "yes", "ump:osmandpri", "0.5"],
+    0xb:  ["highway",  "primary_link", "ump:osmandpri", "0.95"],
+    0xc:  ["junction", "roundabout", "ump:osmandpri", "0.7"],
     0xd:  ["highway",  "cycleway"],
-    0xe:  ["highway",  "tertiary", "tunnel", "yes"],
-    0xf:  ["highway",  "track", "tracktype", "grade4"],
+    0xe:  ["highway",  "tertiary", "tunnel", "yes", "ump:osmandpri", "0.9"],
+    0xf:  ["highway",  "track", "tracktype", "grade4", "ump:osmandpri", "0.5"],
     0x14: ["railway",  "rail"],
     0x15: ["note", "morskie"],  # TODO
     0x16: ["highway",  "path"],
     # 0x16: ["highway",  "pedestrian"],
     0x18: ["waterway", "stream"],
     0x19: ["_rel",     "restriction"],
-    0x1a: ["route",    "ferry"],
-    0x1b: ["route",    "ferry"],
+    0x1a: ["route",    "ferry", "ump:osmandpri", "1.0"],
+    0x1b: ["route",    "ferry", "ump:osmandpri", "1.0"],
     0x1c: ["boundary", "administrative", "admin_level", "8"],
     0x1d: ["boundary", "administrative", "admin_level", "4"],
     0x1e: ["boundary", "administrative", "admin_level", "2"],
@@ -603,7 +603,7 @@ poi_types = {
     0x1615: ["man_made", "beacon", "mark_type", "blue"],
     0x1616: ["man_made", "beacon", "mark_type", "multicolored"],
     0x1708: ["note",     "deadend"],
-    0x1709: ["bridge",   "yes"],
+    0x1709: ["note",     "flyover"],
     0x170b: ["note",     "verify!"],
     0x170f: ["man_made", "beacon", "mark_type", "white"],
     0x1710: ["barrier",  "gate"],
@@ -1531,9 +1531,7 @@ def convert_tags_return_way(mp_record, feat, ignore_errors, filestamp=None, map_
             way['phone'] = value
         elif key == 'HouseNumber':
             way['addr:housenumber'] = value
-        elif key == 'KodPoczt':
-            way['addr:postcode'] = value
-        elif key == 'ZIP':
+        elif key == 'KodPoczt' or key == 'ZIP' or key == 'ZipCode':
             way['addr:postcode'] = value
         elif key == 'Zip':
             pass
@@ -1544,7 +1542,8 @@ def convert_tags_return_way(mp_record, feat, ignore_errors, filestamp=None, map_
         elif key == 'Rozmiar':
             way['Rozmiar'] = value
         elif key == 'ForceSpeed':
-            fspeed = value  # Routing helper
+              # Routing helper
+              way['_ForceSpeed'] = value
         elif key == 'ForceClass':
             fclass = value  # Routing helper
             # Ignore it for the moment, seems to be used mainly for temporary setups
@@ -1931,7 +1930,7 @@ def signbit(x):
         return -1
 
 
-def next_node(pivot=None, direction=None, node_ways_relation=None, map_elements_props=None):
+def next_node(pivot=None, direction=None, node_ways_relation=None, map_elements_props=None, messages_printer=None):
     """
     return either next or previous node relative to the pivot point. In some cases does not do anything as the next
     point is the only one
@@ -1942,7 +1941,7 @@ def next_node(pivot=None, direction=None, node_ways_relation=None, map_elements_
     :return: one node of given road
     """
     way_nodes = nodes_to_way(direction, pivot, node_ways_relation=node_ways_relation,
-                             map_elements_props=map_elements_props)['_nodes']
+                             map_elements_props=map_elements_props, messages_printer=messages_printer)['_nodes']
     pivotidx = way_nodes.index(pivot)
     return way_nodes[pivotidx + signbit(way_nodes.index(direction) - pivotidx)]
 
@@ -1994,7 +1993,7 @@ def name_turn_restriction(rel, nodes, points):
         rel['restriction'] = 'no_u_turn'
 
 
-def preprepare_restriction(rel, node_ways_relation=None, map_elements_props=None):
+def preprepare_restriction(rel, node_ways_relation=None, map_elements_props=None, messages_printer=None):
     """
     modification of relation nodes so, that it starts and ends one node after and one node before central point
     called pivot here. It simplifies calculations as in some cases ways are split then, eg when there are levels.
@@ -2004,25 +2003,27 @@ def preprepare_restriction(rel, node_ways_relation=None, map_elements_props=None
     :return: None, id modifies nodes by reference
     """
     new_rel_node_first = next_node(pivot=rel['_nodes'][1], direction=rel['_nodes'][0],
-                                   node_ways_relation=node_ways_relation, map_elements_props=map_elements_props)
+                                   node_ways_relation=node_ways_relation, map_elements_props=map_elements_props,
+                                   messages_printer=messages_printer)
     new_rel_node_last = next_node(pivot=rel['_nodes'][-2], direction=rel['_nodes'][-1],
-                                  node_ways_relation=node_ways_relation, map_elements_props=map_elements_props)
+                                  node_ways_relation=node_ways_relation, map_elements_props=map_elements_props,
+                                  messages_printer=messages_printer)
     rel['_nodes'][0] = new_rel_node_first
     rel['_nodes'][-1] = new_rel_node_last
 
 
-def prepare_restriction(rel, node_ways_relation=None, map_elements_props=None):
+def prepare_restriction(rel, node_ways_relation=None, map_elements_props=None, messages_printer=None):
     fromnode = rel['_nodes'][0]
     fromvia = rel['_nodes'][1]
     tonode = rel['_nodes'][-1]
     tovia = rel['_nodes'][-2]
     # The "from" and "to" members must start/end at the Role via node or the Role via way(s), otherwise split it!
     split_way(way=nodes_to_way(fromnode, fromvia, node_ways_relation=node_ways_relation,
-                               map_elements_props=map_elements_props),
+                               map_elements_props=map_elements_props, messages_printer=messages_printer),
               splitting_point=fromvia, node_ways_relation=node_ways_relation,
               map_elements_props=map_elements_props)
     split_way(way=nodes_to_way(tonode, tovia, node_ways_relation=node_ways_relation,
-                               map_elements_props=map_elements_props),
+                               map_elements_props=map_elements_props, messages_printer=messages_printer),
               splitting_point=tovia, node_ways_relation=node_ways_relation,
               map_elements_props=map_elements_props)
 
@@ -2047,6 +2048,45 @@ def make_restriction_fromviato(rel, node_ways_relation=None, map_elements_props=
         ways[rel['_members']['from'][1][0]]['_c'] += 1
         ways[rel['_members']['to'][1][0]]['_c'] += 1
     return nodes
+
+
+def make_destination_tag_value(direction_sign):
+    if 'name' in direction_sign and direction_sign['name']:
+        return direction_sign['name']
+    return ''
+
+
+def add_destination_tag_to_way(direction_label, direction_sign, ways, messages_printer=None):
+    # https://wiki.openstreetmap.org/wiki/Key:destination
+    # 'to': ('way', [to_way_index]),
+    if direction_label.startswith('T,') or direction_label.startswith('E,') or direction_label.startswith('O,'):
+        direction_label = direction_label.split(',', 1)[-1]
+    roadsign_to_member = direction_sign['_members']['to']
+    roadsign_via_member = direction_sign['_members']['via']
+    via_node = roadsign_via_member[1][0]
+    to_wayid = roadsign_to_member[1][0]
+    # if the road is oneway road, then we just add destination tag
+    if 'oneway' in ways[to_wayid] and ways[to_wayid]['oneway'] == 'yes':
+        if 'destination' not in ways[to_wayid]:
+            ways[to_wayid]['destination'] = direction_label
+        else:
+            ways[to_wayid]['destination'] = ways[to_wayid]['destination'] + ',' + direction_label
+    else:
+        # if the via_node is the first node of way, then the destination goes into forward direction
+        if via_node == ways[to_wayid]['_nodes'][0]:
+            if 'destination:forward' not in ways[to_wayid]:
+                ways[to_wayid]['destination:forward'] = direction_label
+            else:
+                ways[to_wayid]['destination:forward'] = ways[to_wayid]['destination:forward'] + ',' + direction_label
+        # if the via_node is the last node of way, then the destination goes into forward direction
+        elif via_node == ways[to_wayid]['_nodes'][-1]:
+            if 'destination:backward' not in ways[to_wayid]:
+                ways[to_wayid]['destination:backward'] = direction_label
+            else:
+                ways[to_wayid]['destination:backward'] = ways[to_wayid]['destination:backward'] + ',' + direction_label
+        # otherwise print error
+        else:
+            messages_printer.printerror('Something went wrong, the via_nod is not the first nor last node of a way')
 
 
 def make_multipolygon(outer, holes, filestamp=None, node_ways_relation=None, map_elements_props=None):
@@ -2330,7 +2370,7 @@ def extract_routeparam(value):
     -------
     dict: {key, value}, empty dict in case of failure
     """
-    maxspeeds = {0: '8', 1: '20', 2: '40', 3: '56', 4: '72', 5: '93', 6: '108', 7: '128'}
+    maxspeeds = {0: '8', 1: '20', 2: '40', 3: '56', 4: '72', 5: '93', 6: '120', 7: '140'}
     exceptions = ('emergency', 'goods', 'motorcar', 'psv', 'taxi', 'foot', 'bicycle', 'hgv')
     try:
         params = tuple(int(a) for a in value.split(','))
@@ -2349,6 +2389,56 @@ def extract_routeparam(value):
         if val != 0:
             _way[exceptions[i]] = 'no'
     return _way
+
+
+def get_ump_osmandpri(_way):
+    osmand_pri_modifier = 0.1
+    class_vs_speeds = {0x1:  6,  # highway:motorway
+                      0x2:  5,  # highway:trunk
+                      0x3:  4,  # highway:primary
+                      0x4:  3,  # highway:secondary
+                      0x5:  3,  # highway:tertiary
+                      0x6:  2,  # highway:residential
+                      0x7:  1,  # highway:living_street
+                      0x8:  2,  # highway:trunk_link
+                      0x9:  4,  # highway:motorway_link
+                      0xa:  0,  # highway:track
+                      0xb:  2,  # highway:primary_link
+                      0xc:  1,  # junction:roundabout
+                      0xd:  1,  # highway:cycleway
+                      0xe:  5,   # highway:tertiary tunnel:yes
+                      0xf:  0,   # highway:track tracktype:grade4
+                      0x10:  1,
+                      0x16:  0,   # highway:path
+                      0x1a:  0,  # route:ferry
+                      0x1b:  0,  # route:ferry
+                      }
+    if 'ump:type' not in _way:
+        return None
+    if 'ump:osmandpri' not in _way:
+        return None
+    try:
+        _type = int(_way['ump:type'], 0)
+    except ValueError:
+        return None
+    if 'ump:type' in _way and _type not in class_vs_speeds:
+        return None
+    ump_osmandpri = float(_way['ump:osmandpri'])
+    if '_ForceSpeed' in _way:
+        fs = _way['_ForceSpeed'].lower()
+        try:
+            speed_modifier = int(fs) - class_vs_speeds[_type]
+            ump_osmandpri = ump_osmandpri + ump_osmandpri * speed_modifier * osmand_pri_modifier
+        except ValueError:
+            if fs == 'slower':
+                ump_osmandpri = ump_osmandpri - ump_osmandpri * osmand_pri_modifier
+            elif fs == 'faster':
+                ump_osmandpri = ump_osmandpri + ump_osmandpri * osmand_pri_modifier
+    if ump_osmandpri < 0.4:
+        ump_osmandpri = 0.4
+    elif ump_osmandpri > 1.4:
+        ump_osmandpri = 1.4
+    return str(round(0.05 * round(ump_osmandpri / 0.05), 2))
 
 
 def print_point_pickled(point, pointattr, task_id, orig_id, node_generalizator, ostr):
@@ -2426,6 +2516,8 @@ def print_way_pickled(way, task_id, orig_id, node_generalizator, ostr):
 
 def print_relation_pickled(rel, task_id, orig_id, node_generalizator, ostr):
     """Prints a relation given by rel together with its ID to stdout as XML"""
+    if rel is None:
+        return
     if '_c' in rel:
         if rel['_c'] <= 0:
             return
@@ -2472,6 +2564,7 @@ def post_load_processing(maxtypes=None, progress_bar=None, map_elements_props=No
     pointattrs = map_elements_props['pointattrs']
     relations = OrderedDict()
     levelledways = OrderedDict()
+    rel_vals_for_restrictions = ('restriction', 'lane_restriction', 'roadsign')
     for way_no, way in enumerate(ways):
         if '_rel' in way:
             relations[way_no] = way
@@ -2492,7 +2585,7 @@ def post_load_processing(maxtypes=None, progress_bar=None, map_elements_props=No
                 if maxtypes[i] < maxtype:
                     maxtype = maxtypes[i]
             tag(way, pline_types[maxtype])
-            if 'oneway' in way:
+            if 'junction' in way and way['junction'] == 'roundabout' and 'oneway' in way:
                 del way['oneway']
             # TODO make sure nodes are ordered counter-clockwise
 
@@ -2516,10 +2609,10 @@ def post_load_processing(maxtypes=None, progress_bar=None, map_elements_props=No
     for way_id, rel in relations.items():
         _line_num += 1
         progress_bar.set_val(_line_num, 'drp')
-        if rel['type'] in ('restriction', 'lane_restriction',):
+        if rel['type'] in rel_vals_for_restrictions:
             try:
                 preprepare_restriction(rel, node_ways_relation=node_ways_relation,
-                                       map_elements_props=map_elements_props)
+                                       map_elements_props=map_elements_props, messages_printer=messages_printer)
                 # print "DEBUG: preprepare_restriction(rel:%r) OK." % (rel,)
             except NodesToWayNotFound:
                 messages_printer.printerror("warning: Unable to find nodes to preprepare restriction from rel: %r\n"
@@ -2580,21 +2673,28 @@ def post_load_processing(maxtypes=None, progress_bar=None, map_elements_props=No
     for rel in map_elements_props['relations']:
         _line_num += 1
         progress_bar.set_val(_line_num, 'drp')
-        if rel['type'] in ('restriction', 'lane_restriction',):
+        if rel['type'] in rel_vals_for_restrictions:
             try:
-                prepare_restriction(rel, node_ways_relation=node_ways_relation, map_elements_props=map_elements_props)
+                prepare_restriction(rel, node_ways_relation=node_ways_relation, map_elements_props=map_elements_props,
+                                    messages_printer=messages_printer)
             except NodesToWayNotFound:
                 messages_printer.printerror("warning: Unable to find nodes to prepare restriction from rel: %r\n" % rel)
 
-    for rel in map_elements_props['relations']:
+    for rel_num, rel in enumerate(map_elements_props['relations']):
+        # rel = map_elements_props['relations'][rel_num]
         _line_num += 1
         progress_bar.set_val(_line_num, 'drp')
-        if rel['type'] in ('restriction', 'lane_restriction',):
+        if rel['type'] in rel_vals_for_restrictions:
             try:
                 rnodes = make_restriction_fromviato(rel, node_ways_relation=node_ways_relation,
                                                     map_elements_props=map_elements_props, )
                 if rel['type'] == 'restriction':
                     name_turn_restriction(rel, rnodes, map_elements_props['points'])
+                elif rel['type'] == 'roadsign':
+                    dir_tag = make_destination_tag_value(rel)
+                    if dir_tag:
+                        add_destination_tag_to_way(dir_tag, rel, ways, messages_printer=messages_printer)
+                    map_elements_props['relations'][rel_num] = None
             except NodesToWayNotFound:
                 messages_printer.printerror("warning: Unable to find nodes to preprepare restriction from rel: %r\n"
                                             % rel)
@@ -2602,14 +2702,22 @@ def post_load_processing(maxtypes=None, progress_bar=None, map_elements_props=No
     # Quirks, but do not overwrite specific values
     for way in ways:
         _line_num += 1
+        progress_bar.set_val(_line_num, 'drp')
         if way is None:
             continue
-        progress_bar.set_val(_line_num, 'drp')
-        if 'highway' in way and 'maxspeed' not in way:
-            if way['highway'] == 'motorway':
-                way['maxspeed'] = '140'
-            if way['highway'] == 'trunk':
-                way['maxspeed'] = '120'
+        if 'highway' in way:
+            ump_osmandpri = get_ump_osmandpri(way)
+            # maxspeed = get_maxspeed_for_road(way)
+            if ump_osmandpri is None:
+                continue
+            way['ump:osmandpri'] = ump_osmandpri
+            if '_ForceSpeed' in way and way['_ForceSpeed'].lower() == 'slower':
+                way['surface'] = 'gravel'
+            if 'maxspeed' not in way:
+                if way['highway'] == 'motorway':
+                    way['maxspeed'] = '140'
+                if way['highway'] == 'trunk':
+                    way['maxspeed'] = '120'
 
     for index, point in pointattrs.items():
         _line_num += 1
@@ -3101,6 +3209,8 @@ def main(options, args):
     bpoints = None
 
     sys.stderr.write("INFO: mdmMp2xml.py ver:" + __version__ + " ran at " + get_runstamp() + "\n")
+    if options.road_signs:
+        pline_types[0x2f] = ["_rel", "roadsign"]
     if options.threadnum > 32:
         options.threadnum = 32
     if options.threadnum < 1:
@@ -3293,7 +3403,8 @@ def main(options, args):
                 messages_printer.printinfo_nlf("\n\tERROR: Normal output failed!\n")
                 sys.exit()
         elapsed = datetime.now().replace(microsecond=0) - runtime
-        messages_printer.printinfo("mdmMp2xml.py finished after " + str(elapsed) + ".\n")
+        messages_printer.printinfo("mdmMp2xml.py finished at " + time.strftime("%Y-%m-%dT%H:%M:%SZ") +
+                                   " after: " + str(elapsed) + ".\n")
     else:
         sys.stderr.write("\tINFO: Border file required when more than one area given: --border \n")
 
@@ -3334,6 +3445,8 @@ if __name__ == '__main__':
     parser.add_option("--regions",
                       action="store_true", dest="regions", default=False,
                       help="attach regions to cities in the index file")
+    parser.add_option("--add-roadsigns", dest="road_signs", default=False, action="store_true",
+                      help="create roadsigns from 0x2f lines")
     parser.add_option('--monoprocess_outputs', dest="monoprocess_outputs", default=False, action='store_true',
                       help="generate outputs in single process, do not use multiprocessing")
     parser.add_option('--force_timestamp', dest='force_timestamp', type='string', action='store',
