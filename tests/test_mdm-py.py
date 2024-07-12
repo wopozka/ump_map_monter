105b82cdd6d6932317c3a01157272b137fa1460c
Piotr Wawrzyniak <wopozka@gmail.com>
2024-07-12 14:56:55 +0200

    dodajemy testcase dla mdm-py

---
+++ tests/test_mdm-py.py
@@ -0,0 +1,9 @@
+import pytest
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+# from mdm-py import cvs_sprawdz_czy_tylko_dozwolone_klucze_i_brak_konfliktow
+
+CZY_KLUCZE_OK_I_BRAK_KONFLIKTOW = (
+    ()
+)
